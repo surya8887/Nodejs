236651afa4f7b3a22b4e763e70657d34ecb8657b
vijay kumar <vijayabout2001@gmail.com>
2026-01-21 08:07:36 +0530

    basic node js server file

---
+++ basic-nodeJS/myfirst.js
@@ -0,0 +1,21 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    res.end('Hello World\n');
+})
+
+const PORT = 3000;
+server.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}/`);
+});
+
+/*
+nodejs basic-commands:
+1. node myfirst.js  --> to run the file
+2. ctrl + c        --> to stop the server
+2. node --version = node -v  --> to check nodejs version
+3. npm --version = npm -v   --> to check npm version
+
+*/  
